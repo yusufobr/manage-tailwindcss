3aebf01da95145a8c2807e2540d1a8eb1a449e6f
yusufobr <yousarsif80@gmail.com>
2022-12-23 14:26:53 +0100

    last commit

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,6 @@ const nav = document.getElementById('menu')
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
-    nav.classList.toggle('flex')
+    // nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
